6cb3341e82fb561a210b67563288b4839dd22c4d
boristestov <boris.testov@gmail.com>
2020-11-07 22:19:57 +0300

    Test button added

--- cmake/executables/executables.cmake
+++ cmake/executables/executables.cmake
@@ -1,9 +1,20 @@
 include_directories(${INCLUDE_DIR})
 
-file (GLOB SOURCES "${SRC_DIR}/*.cpp")
-file (GLOB HEADERS "${INCLUDE_DIR}/*.h" "${INCLUDE_DIR}/*.hpp")
+set(Boost_USE_STATIC_LIBS OFF)
+set(Boost_USE_MULTITHREADED ON)
+set(Boost_USE_STATIC_RUNTIME OFF)
+set(BOOST_INCLUDEDIR /usr/include/boost)
+set(BOOST_LIBRARYDIR /usr/lib)
+find_package(Boost 1.70.0 COMPONENTS program_options filesystem regex REQUIRED)
+include_directories(${Boost_INCLUDE_DIRS})
+
+find_package(Qt5 REQUIRED COMPONENTS Core Gui Widgets)
+
+file(GLOB SOURCES "${SRC_DIR}/*.cpp")
+file(GLOB HEADERS "${INCLUDE_DIR}/*.h" "${INCLUDE_DIR}/*.hpp")
 
 add_executable(${PROJECT_NAME} ${SOURCES} ${HEADERS})
+target_link_libraries(${PROJECT_NAME} ${Boost_LIBRARIES} Qt5::Core Qt5::Gui Qt5::Widgets)
 set_target_properties(${PROJECT_NAME} PROPERTIES ${TARGET_PROPERTIES})
 target_compile_options(${PROJECT_NAME} PRIVATE ${COMPILE_OPTIONS})
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "version.h"
+
+#include <QApplication>
+#include <QPushButton>
 #include <iostream>
 #include <map>
 #include <vector>
 
-int main() {
-  std::cout << "Current version: " << version() << std::endl;
-  return 0;
+int main(int argc, char** argv)
+{
+    std::cout << "Current version: " << version() << std::endl;
+    QApplication app(argc, argv);
+    QPushButton button("Hello world !");
+    button.setText("My text");
+    button.setToolTip("A tooltip");
+    button.show();
+    return app.exec();
+    return 0;
 }
